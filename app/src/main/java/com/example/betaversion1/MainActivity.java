c4befe1e20448fbcc74bd465982e8f45633f8fa4
NoamVak <61119967+NoamVak@users.noreply.github.com>
2022-06-14 14:11:54 +0300

    need to continue

--- app/src/main/java/com/example/betaversion1/MainActivity.java
+++ app/src/main/java/com/example/betaversion1/MainActivity.java
@@ -6,7 +6,10 @@ import static com.example.betaversion1.FBref.refSales;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -24,91 +27,52 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
     FirebaseUser user;
-    Sales sale, sale2;
-    Books book, book2;
-    ArrayList<String> lst = new ArrayList<>();
-    ArrayList<String> bookList = new ArrayList<String>();
-    ArrayList<Books> bookValues = new ArrayList<Books>();
-    ArrayList<String> saleList = new ArrayList<String>();
-    ArrayList<Sales> saleValues = new ArrayList<Sales>();
-    String uid, str1;
-    boolean status = true;
-    int genre = 2;
-    ListView listview, listview1;
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listview = (ListView) findViewById(R.id.listview);
-        listview1 = (ListView) findViewById(R.id.listview1);
         refBooks.push();
 
-        lst.add("926SUu2pP4ODVcspD0gretH7Jzh1-0");
         user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, getEmail or etc
             uid = user.getUid();
         }
-        sale = new Sales("20220607", status, 30, 5, lst, uid, "1", "sale check");
-        refSales.child(String.valueOf(status)).setValue(sale);
-
-        book = new Books("Harry Potter", "J.k Rowling", genre, lst, 340, "1", 2, "Book Check");
-        refBooks.child(String.valueOf(genre)).setValue(book);
-
-        ArrayAdapter<String> saleAdp = new ArrayAdapter<String>(this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, saleList);
-
-        ValueEventListener saleListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dS) {
-                saleList.clear();
-                saleValues.clear();
-                for (DataSnapshot data : dS.getChildren()) {
-                    str1 = (String) data.getKey();
-                    Sales saleTmp = data.getValue(Sales.class);
-                    saleValues.add(saleTmp);
-                    saleList.add(str1);
-                }
-                saleAdp.notifyDataSetChanged();
-                listview.setAdapter(saleAdp);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-        refSales.addValueEventListener(saleListener);
-
-
-        ArrayAdapter<String> bookAdp = new ArrayAdapter<String>(this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, bookList);
-
-        ValueEventListener bookListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dS) {
-                bookList.clear();
-                bookValues.clear();
-                for (DataSnapshot data : dS.getChildren()) {
-                    str1 = (String) data.getKey();
-                    Books bookTmp = data.getValue(Books.class);
-                    bookValues.add(bookTmp);
-                    bookList.add(str1);
-                }
-                bookAdp.notifyDataSetChanged();
-                listview1.setAdapter(bookAdp);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-        refBooks.addValueEventListener(bookListener);
 
     }
+
+    public void profile(View view){
+        Intent si=new Intent(MainActivity.this, Profile.class);
+        startActivity(si);
+    }
+
+    public void createSale(View view) {
+        Intent si=new Intent(MainActivity.this, CreateSale.class);
+        startActivity(si);
+    }
+
+    public void shop(View view) {
+        Intent si=new Intent(MainActivity.this, Shop.class);
+        startActivity(si);
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        String st=item.getTitle().toString();
+        if(st.equals("Sign Out")){
+        }
+        return true;
+    }*/
+
+
+
 
 
 
