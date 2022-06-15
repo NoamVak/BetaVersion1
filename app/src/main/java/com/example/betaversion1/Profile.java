bc8dd0221b30485d653b347f207a68bc5e84f600
NoamVak <61119967+NoamVak@users.noreply.github.com>
2022-06-15 17:35:49 +0300

    Edit profile works without changing profile picture need to continue

--- app/src/main/java/com/example/betaversion1/Profile.java
+++ app/src/main/java/com/example/betaversion1/Profile.java
@@ -29,6 +29,7 @@ public class Profile extends AppCompatActivity {
     String str1;
     ImageView profilePic;
     TextView tvUsername,tvBio;
+
     FirebaseUser user;
     String uid;
     ArrayList<String> userList=new ArrayList<String>();
